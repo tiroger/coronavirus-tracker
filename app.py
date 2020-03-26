3552f268c032a3f39627a74dbdfaaaa673a09dbb
Roger Lefort <tirogerl@gmail.com>
2020-03-25 22:43:35 -0400

    Updated data URLs

--- app.py
+++ app.py
@@ -23,9 +23,9 @@ def loadData(fileName, columnName):
     return data
 
 
-all_data = loadData("time_series_19-covid-Confirmed.csv", "CumConfirmed") \
-    .merge(loadData("time_series_19-covid-Deaths.csv", "CumDeaths")) \
-    .merge(loadData("time_series_19-covid-Recovered.csv", "CumRecovered"))
+all_data = loadData("time_series_covid19_confirmed_global.csv", "CumConfirmed") \
+    .merge(loadData("time_series_covid19_deaths_global.csv", "CumDeaths")) \
+    .merge(loadData("time_series_covid19_recovered_global.csv", "CumRecovered"))
 
 countries = sorted(all_data['Country/Region'].unique())
 
